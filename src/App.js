0a85c327602a0afa2de83be20c925c5c698f1f58
suedenim <suedenim@users.noreply.github.com>
2020-04-09 10:46:39 -0700

    Start of Section 7

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import classes from './App.css';
 import Person from './Person/Person';
-import ValidationComponent from './UserInputOutput/ValidationComponent';
-import CharComponent from './UserInputOutput/CharComponent';
 
 class App extends Component {
   state = {
     persons: [
-      { id: '1', name: 'Max', age: 28 },
-      { id: '2', name: 'Manu', age: 29 },
-      { id: '3', name: 'Stephanie', age: 26 }
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
     ],
-    showPersons: false,
     otherState: 'some other value',
-    textLength: 0,
-    typedText: '',
-  }
-
-  deletePersonHandler = (personIndex) => {
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  }
-
-  deleteLetterHandler = (letterIndex) => {
-    const typedText = [...this.state.typedText];
-    typedText.splice(letterIndex, 1);
-    this.setState({typedText: typedText.join('')});
-  }
+    showPersons: false
+  };
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -37,103 +22,69 @@ class App extends Component {
     const person = {
       ...this.state.persons[personIndex]
     };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( { persons: persons } )
-  }
+    this.setState({ persons: persons });
+  };
 
-  textChangedHandler = (event) => {
-    this.setState({textLength: event.target.value.length});
-    this.setState({typedText: event.target.value});
-    console.log(this.state.typedText);
-  }
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
-  togglePersonHandler = () => {
+  togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
-    
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
-  render () {
-    // const style = {
-    //   backgroundColor: 'green',
-    //   color: 'white',
-    //   font: 'inherit',
-    //   border: '1px solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer'
-    // };
-
+  render() {
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
-      <div >
-        {
-          this.state.persons.map((person, idx) => {
-            return <Person 
-              name={person.name} 
-              age={person.age} 
-              key={person.id} 
-              click={() => this.deletePersonHandler(idx)}
-              changed={(event) => this.nameChangedHandler(event, person.id)}/>
-          })
-        }
-
-      </div>
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
       );
-      //style.backgroundColor = 'red';
+
+      btnClass = classes.Red;
     }
 
-    let chars = null;
-    if (this.state.typedText.length > 0) {
-      let charArray = this.state.typedText.split('');
-      chars = (
-      <div >
-        {
-          charArray.map((char, idx) => {
-            return <CharComponent 
-              character={char + ' ' + idx} 
-              key={idx}
-              click={() => this.deleteLetterHandler(idx)} />
-          })
-        }
-
-      </div>
-      );
+    const assignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button alt={this.state.showPersons}
-        className = "button"
-          //style={style}
-          onClick={this.togglePersonHandler}>Toggle Persons
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
+          Toggle Persons
         </button>
-        
         {persons}
-
-        <p>
-          <input 
-            type="text" 
-            value={this.state.typedText}
-            onChange={(event) => this.textChangedHandler(event)} />
-          <br />
-          <span>{this.state.textLength}</span>
-        </p>
-
-        <div>
-          <ValidationComponent
-            minLength={5}
-            textLength={this.state.textLength} />
-        </div>
-
-        {chars}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
