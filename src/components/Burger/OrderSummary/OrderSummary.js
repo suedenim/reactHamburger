95464c3774f3bdc0c4cabfeb1a218be5d8bc4a67
suedenim <suedenim@users.noreply.github.com>
2020-11-23 18:22:46 -0800

    Added order modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Aux from '../../../hoc/Auxilliary';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+            <li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+            </li>);
+        });
+
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delish burger with these ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Aux>
+    );
+};
+
+export default orderSummary;
